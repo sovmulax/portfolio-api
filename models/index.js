2bbb92e09fc7736bba76b6c4bbf1a69ba3be8588
sovmulax <soumailaevane@gmail.com>
2023-04-01 19:01:56 +0000

    re re

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 'use strict';
-require('dotenv').config();
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
@@ -10,7 +10,11 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-sequelize = new Sequelize(config.database, config.username, config.password, config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
