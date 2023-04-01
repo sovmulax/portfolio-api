6157269379a17efeccab0f74a3cd09a9a89045ab
sovmulax <soumailaevane@gmail.com>
2023-04-01 16:58:29 +0000

    conatcts model set

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,33 +1,44 @@
-'use strict';
-const {Model} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class Clients extends Model {}
-  Clients.init({
-    nom: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      unique: true
+  class Contacts extends Model {}
+  Contacts.init(
+    {
+      nom: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      phone: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      subject: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
+      message: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        unique: true,
+      },
     },
-    email: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      unique: true,
-      validate:{
-        isEmail: true
-      }
-    },
-    tel: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      unique: true
-    },
-    avatar: {
-      type: DataTypes.TEXT,
-    },  }, {
-    sequelize,
-    modelName: 'clients',
-    paranoid: true,
-    deletedAt: 'destroyTime',
-  });
-  return Clients;
+    {
+      sequelize,
+      modelName: "contacts",
+      paranoid: true,
+      deletedAt: "destroyTime",
+    }
+  );
+  return Contacts;
 };
